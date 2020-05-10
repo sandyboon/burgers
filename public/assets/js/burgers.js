018a0f932064179d81bc40b9a06c44649fe641d5
ssingh <sandy.boon@gmail.com>
2020-05-10 19:17:35 -0400

    added front end validation for burger name

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,31 +2,36 @@
 
 //attach a click listener to add burger button
 $('#addBurger').click(addABurger);
+//attach a click listener to devour burger buttons
+$('.devourBtn').click(devourABurger);
 
 //Enable the button when there is some text in the textbox
 
 $('#tobeadded').on('input', function () {
-  //enable the button
-  console.log('changed');
-  let burgerName = $('#tobeadded').val();
-  console.log(burgerName);
-  if (burgerName.trim().length > 0) {
-    console.log('removing');
-    console.log($('#tobeadded'));
-    $('#tobeadded').removeAttr('disabled');
-  }
+  $('#tobeadded').next().removeClass();
+  $('#tobeadded').next().addClass('hideError');
 });
 
 function addABurger() {
   let burgerName = $('#tobeadded').val();
-  //call the api and post the new burger
-  $.post('/api/burgers', {
-    burger_name: burgerName,
-    devoured: false,
-  }).done(function (response) {
-    //add the burger to the list of availabe burgers
-    makeBurgerAvailable(response.data);
-  });
+  if (burgerName.trim().length <= 0) {
+    $('#tobeadded').next().removeClass();
+    $('#tobeadded').next().addClass('showError');
+  } else {
+    //call the api and post the new burger
+    $.post('/api/burgers', {
+      burger_name: burgerName,
+      devoured: false,
+    }).done(function (response) {
+      //add the burger to the list of availabe burgers
+      makeBurgerAvailable(response.data);
+    });
+  }
+}
+
+function devourABurger({ target }) {
+  console.log(target.id + target.getAttribute('data-burger-name'));
+  //call the api to update the burger
 }
 
 function makeBurgerAvailable(burger) {
