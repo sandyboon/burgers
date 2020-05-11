b2f22c480c43497a91753256bbd95992acf3a487
ssingh <sandy.boon@gmail.com>
2020-05-10 21:26:10 -0400

    added functionality for devouring a burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -23,6 +23,8 @@ function addABurger() {
       burger_name: burgerName,
       devoured: false,
     }).done(function (response) {
+      //empty the input box
+      $('#tobeadded').val('');
       //add the burger to the list of availabe burgers
       makeBurgerAvailable(response.data);
     });
@@ -30,22 +32,70 @@ function addABurger() {
 }
 
 function devourABurger({ target }) {
-  console.log(target.id + target.getAttribute('data-burger-name'));
+  console.log('Devouring burger: ');
+  let burger = getBurgerFromBtnTarget(target);
   //call the api to update the burger
+  burgerDevourer(burger);
+}
+
+function burgerDevourer(burger) {
+  const updateBurger = {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(burger),
+  };
+  fetch('/api/burgers/', updateBurger)
+    .then((response) => response.json())
+    .then(function ({ data }) {
+      //remove from available burgers first
+      moveBurgerToDevouredBurgers(data);
+    });
+}
+
+function moveBurgerToDevouredBurgers(data) {
+  console.log('Adding to right');
+  $('#' + data.id)
+    .parents('.burgerRow')
+    .remove();
+  document.getElementById('devouredBurgers').insertAdjacentHTML(
+    'beforeend',
+    `
+            <div class="row">
+            <div class="col-6 offset-3">
+                <span class="dvouredBurger">
+                    <s>${data.burger_name}</s>
+                </span>
+            </div>
+        </div>
+            `
+  );
+}
+
+function getBurgerFromBtnTarget(target) {
+  return {
+    burger_name: target.getAttribute('data-burger-name'),
+    devoured: true,
+    id: target.id,
+  };
 }
 
 function makeBurgerAvailable(burger) {
   document.getElementById('allAvailableBurgers').insertAdjacentHTML(
     'beforeend',
     `
-        <div class="row mt-2">
+        <div id="${burger.id}burgerRow" class="row mt-2 burgerRow">
             <div class="col-6">
                 <span class="availableBurger">${burger.burger_name}</span>
             </div>
             <div class="col-5">
-                <button style="display: inline;" id="${burger.id}">Devour it!</button>
+                <button data-burger-name="${burger.burger_name}" class="devourBtn" style="display: inline;"
+                id="${burger.id}">Devour it!</button>
             </div>
         </div>
     `
   );
+  //add the click lisnter to be devoured
+  $(`#${burger.id}burgerRow`).click(devourABurger);
 }
